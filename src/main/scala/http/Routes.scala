be672364799fc23e6dd6eac04324605dd788c361
aciobanu196 <alin.m.ciobanu@gmail.com>
2019-11-11 11:56:39 +0200

    Making changes so that the project is as it should be  - Added comments to some of the classes pointing what I think it needs to be done

---
+++ src/main/scala/http/Routes.scala
@@ -0,0 +1,41 @@
+package http
+
+//import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport - // Deprecated not available on Scala 2.13.0 need to have a closer look at circe :(
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes._
+import DBrelated.Products
+//import Services.ShopService // Deprecated as well, used for testing purposes with Scala 2.12.1
+//import spray.json.DefaultJsonProtocol
+
+// The paths should be correct just need to find the proper way tu use the JSON formatter
+
+//class Routes(name:ShopService) {
+//
+//  trait Protocols extends SprayJsonSupport with DefaultJsonProtocol {
+//    implicit val shopFormat = jsonFormat4(Products)
+//
+//    class ProductRoutes(val shopService: ShopService) extends Protocols {
+//
+//      val route = logRequestResult("ProductRoutes") {
+//        pathPrefix("products") {
+//          pathEndOrSingleSlash {
+//            get {
+//              complete {
+//                shopService.getProducts()
+//              }
+//            } ~
+//              post {
+//                entity(as[Products]) { productForCreate =>
+//                  complete {
+//                    shopService.createProduct(productForCreate)
+//                  }
+//                }
+//              }
+//          }
+//        }
+//      }
+//    }
+//
+//  }
+//
+//}
