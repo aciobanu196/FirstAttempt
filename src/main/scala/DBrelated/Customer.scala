d9ad18b5ad183b9578a9bf40d249a3f96445f414
aciobanu196 <alin.m.ciobanu@gmail.com>
2019-11-14 18:10:08 +0200

    Some small in the application.conf file

--- src/main/scala/DBrelated/Customer.scala
+++
@@ -1,9 +0,0 @@
-//package DBrelated
-//
-//case class Customer(id: Option[Long] = None,
-//                    customerName: String,
-//                    customerEmail: String,
-//                    customerAdress: String)
-
-
-//PLACEHOLDER
--- src/main/scala/DBrelated/Products.scala
+++
@@ -1,9 +0,0 @@
-//package DBrelated
-//
-//case class Products(id: Option[Long] = None,
-//                    product_name: String,
-//                    product_price: String,
-//                    product_type: String)
-
-
-//PLACEHOLDER
