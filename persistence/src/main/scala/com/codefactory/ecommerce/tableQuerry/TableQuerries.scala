cac717bb95c0af36936f03897b885db36b527d3b
aciobanu196 <alin.m.ciobanu@gmail.com>
2019-11-18 15:40:44 +0200

    Adding types of sorting forproducts with types- hopefully this is what was desired

--- persistence/src/main/scala/com/codefactory/ecommerce/tableQuerry/TableQuerries.scala
+++ persistence/src/main/scala/com/codefactory/ecommerce/tableQuerry/TableQuerries.scala
@@ -18,26 +18,30 @@ object TableQuerries extends App with QuerryVariable with TableVariables {
     (p, t) <- products join types on (_.typeID === _.id)
   } yield (p.id, p.name, p.price, p.quantity, t.tname)
 
-  val action = productsWithType.sortBy(_._5.desc).result
-  val result = db.run(action)
-  Await.result(result, Duration.Inf)
-  result.foreach(println)
+  val actionDesc = productsWithType.sortBy(_._5.desc).result
+  val actionAsc  = productsWithType.sortBy(_._5.asc).result
+  val resultDesc = db.run(actionDesc)
+  val resultAsc  = db.run(actionAsc)
+  Await.result(resultDesc, Duration.Inf)
+  Await.result(resultAsc, Duration.Inf)
+  resultDesc.foreach(println)
+  resultAsc.foreach(println)
 
   // Don't know yet if this is exactly how it will be used/ need to further think at it - Need to add up the total
-  val productToCart = for {
-    (c, p) <- carts join products on (_.productID === _.id)
-  } yield (c.id, c.quantity, c.status, c.quantity, p.price)
-
-  // Join to bring the balance for a specific user
-
-  val bringBalanceForUser = for {
-    (u, b) <- users join banks on (_.bankID === _.id)
-  } yield
-    (u.id, b.balance) // Might need some extra work - I mean I need that the balance for each individual user to be returned
-  val action2 = bringBalanceForUser.result
-  val result2 = db.run(action2)
-  Await.result(result2, Duration.Inf)
-  result2.foreach(println)
+//  val productToCart = for {
+//    (c, p) <- carts join products on (_.productID === _.id)
+//  } yield (c.id, c.quantity, c.status, c.quantity, p.price)
+//
+//  // Join to bring the balance for a specific user
+//
+//  val bringBalanceForUser = for {
+//    (u, b) <- users join banks on (_.bankID === _.id)
+//  } yield
+//    (u.id, b.balance) // Might need some extra work - I mean I need that the balance for each individual user to be returned
+//  val action2 = bringBalanceForUser.result
+//  val result2 = db.run(action2)
+//  Await.result(result2, Duration.Inf)
+//  result2.foreach(println)
 
 //  val userId =
 //    (users returning users.map(_.id)) += User(3,
